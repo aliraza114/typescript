72bc78f617efb22c4e16339201eddecd49cdf257
aliraza <aliraza3569@gmail.com>
2020-07-27 11:30:49 +0500

    settings for projects

--- app.ts
+++ app.ts
@@ -4,15 +4,22 @@ function add(n1: number, n2: number) {
 }
 // add(4, 9)
 
-function printResult(res: number){
-    console.log('Result : '+ res)
+function printResult(res: number) {
+    console.log('Result : ' + res)
 }
 
-printResult(add(5, 7))
-function addAndHandle(num: number, numb: number, cd: (res: number) => void){
+//printResult(add(5, 7))
+function addAndHandle(num: number, numb: number, cd: (res: number) => void) {
     const re = num + numb
     cd(re)
 }
-addAndHandle(10, 40, (result) => {
-    console.log('callback ', result)
-})
+// addAndHandle(10, 40, (result) => { console.log('callback ', result) })
+
+function generateError(message: string, errorCode: number) {
+    throw {
+        message: message,
+        errorCode: errorCode
+    }
+}
+
+generateError('this is not hap at its very best', 500)
--- basics.ts
+++
@@ -1,6 +0,0 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
-}
-
-let res = add(4,6)
-console.log(res)
